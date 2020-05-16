b5d6c78c3b1b734c4890fcd7072ff457cc608e4d
vnqx <accs.x@pm.me>
2020-05-16 23:03:59 +0200

    Add express and setup a basic server

---
+++ server.ts
@@ -0,0 +1,21 @@
+import { ApolloServer } from "apollo-server-express";
+import context from "./context";
+import { schema } from "./schema";
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+const server = new ApolloServer({
+  schema,
+  context,
+  playground: true,
+  introspection: true,
+});
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen({ port: PORT }, () => {
+  console.log(`Server ready at http://localhost:${PORT}`);
+});
