d5c446d0635aafb39751411268457e550c09dba3
vnqx <accs.x@pm.me>
2020-05-16 21:22:07 +0200

    Introspect and rename fields slightly

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "public"."User" (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255)
+  name VARCHAR(255),
   email VARCHAR(255) UNIQUE NOT NULL
-)
+);
 
 CREATE TABLE "public"."Post" (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -12,11 +12,11 @@ CREATE TABLE "public"."Post" (
   published BOOLEAN NOT NULL DEFAULT false,
   "authorId" INTEGER NOT NULL,
   FOREIGN KEY ("authorId") REFERENCES "public"."User"(id)
-)
+);
 
 CREATE TABLE "public"."Profile" (
   id SERIAL PRIMARY KEY NOT NULL,
   bio TEXT,
   "userId" INTEGER UNIQUE NOT NULL,
   FOREIGN KEY ("userId") REFERENCES "public"."User"(id)
-)
+);
