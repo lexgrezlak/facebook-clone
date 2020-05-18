470968aa1b637e0f3b83f8fce0191e98c37911ca
vnqx <accs.x@pm.me>
2020-05-18 04:48:13 +0200

    Add FriendStatus to the types

--- api/src/types.ts
+++ api/src/types.ts
@@ -3,5 +3,6 @@ import { Query } from "./types/Query";
 import { Mutation } from "./types/Mutation";
 import { Post } from "./types/Post";
 import { User } from "./types/User";
+import { FriendStatus } from "./types/FriendStatus";
 
-export default [Query, Mutation, AuthPayload, Post, User];
+export default [Query, Mutation, AuthPayload, Post, User, FriendStatus];
