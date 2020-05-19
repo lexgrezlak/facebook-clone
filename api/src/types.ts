66c324de229cd5215eb3627d9daf6d6a34780071
vnqx <accs.x@pm.me>
2020-05-20 01:01:43 +0200

    Remove auth payload

--- api/src/types.ts
+++ api/src/types.ts
@@ -1,8 +1,7 @@
-import { AuthPayload } from "./types/AuthPayload";
 import { Query } from "./types/Query";
 import { Mutation } from "./types/Mutation";
 import { Post } from "./types/Post";
 import { User } from "./types/User";
 import { FriendStatus } from "./types/FriendStatus";
 
-export default [Query, Mutation, AuthPayload, Post, User, FriendStatus];
+export default [Query, Mutation, Post, User, FriendStatus];
