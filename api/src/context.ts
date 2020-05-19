b7f389b5657a5c0d89a36945d741fef1823f227b
vnqx <accs.x@pm.me>
2020-05-20 00:56:35 +0200

    Add response to context

--- api/src/context.ts
+++ api/src/context.ts
@@ -5,8 +5,9 @@ const prisma = new PrismaClient();
 export interface Context {
   prisma: PrismaClient;
   req: any;
+  res: any;
 }
 
-export const context = ({ req }: any): Context => {
-  return { req, prisma };
+export const context = ({ req, res }: any): Context => {
+  return { req, res, prisma };
 };
