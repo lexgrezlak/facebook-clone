151be374347dbec72db2d95fed54b3d3b2514644
John <accs.x@pm.me>
2020-06-28 17:20:39 +0200

    uibuild

--- packages/server/src/utils/createConnection.ts
+++ packages/server/src/utils/createConnection.ts
@@ -1,7 +1,32 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import {
+  createConnection,
+  getConnectionOptions,
+  ConnectionOptions,
+} from "typeorm";
 import { NODE_ENV } from "../config";
 
+async function getOptions() {
+  let connectionOptions: ConnectionOptions;
+  connectionOptions = {
+    type: "postgres",
+    synchronize: false,
+    logging: false,
+    extra: {
+      ssl: true,
+    },
+    entities: ["dist/entity/**/*.ts"],
+  };
+
+  if (process.env.DATABASE_URL) {
+    Object.assign(connectionOptions, { url: process.env.DATABASE_URL });
+  } else {
+    connectionOptions = await getConnectionOptions(NODE_ENV);
+  }
+
+  return connectionOptions;
+}
+
 export const createTypeORMConnection = async () => {
-  const options = await getConnectionOptions(NODE_ENV);
-  return createConnection({ ...options, name: "default" });
+  const typeormConfig = await getOptions();
+  return createConnection({ ...typeormConfig, name: "default" });
 };
